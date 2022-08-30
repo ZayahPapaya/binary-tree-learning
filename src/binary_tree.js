fccf12db12654aa6e1749f565ee43bc0011df62b
Aiden <hyena.shenanigans@gmail.com>
2022-08-29 23:30:40 -0700

    Adds binary tree methods

--- src/binary_tree.js
+++ src/binary_tree.js
@@ -1,23 +1,3 @@
-// function preOrder(root) {
-//   // Root, Left, Right
-//   // Return a single array
-//   let traversal = [];
-//   traversal.push(root.value); // Root
-
-//   if (root.left) {
-//     // Left
-//     let leftTraversal = preOrder(root.left);
-//     traversal = traversal.concat(leftTraversal);
-//   }
-
-//   // Right
-//   if (root.right) {
-//     let rightTraversal = preOrder(root.right);
-//     traversal = traversal.concat(rightTraversal);
-//   }
-
-//   return traversal;
-// }
 
 const preOrder = (root) => root ? [root.value, ...preOrder(root.left), ...preOrder(root.right)] : [];
 const inOrder = (root) => root ? [...inOrder(root.left), root.value, ...inOrder(root.right)] : [];
@@ -29,29 +9,7 @@ class Node {
     this.left = left; // A Node
     this.right = right; // A Node
   }
-
-  // preOrder() {
-  //   // Root, Left, Right
-  //   // Return a single array
-  //   let traversal = [];
-  //   traversal.push(this.value); // Root
-
-  //   if (this.left) {
-  //     // Left
-  //     let leftTraversal = this.left.preOrder();
-  //     traversal = traversal.concat(leftTraversal);
-  //   }
-
-  //   // Right
-  //   if (this.right) {
-  //     let rightTraversal = this.right.preOrder();
-  //     traversal = traversal.concat(rightTraversal);
-  //   }
-
-  //   return traversal;
-  // }
   preOrder = () => this.value ? [this.value, ...preOrder(this.left), ...preOrder(this.right)] : []; // root left right
-
   inOrder = () => this.value ? [...inOrder(this.left), this.value, ...inOrder(this.right)] : []; // left root right
   postOrder = () => this.value ? [...postOrder(this.left), ...postOrder(this.right), this.value] : []; // left right root
 }
